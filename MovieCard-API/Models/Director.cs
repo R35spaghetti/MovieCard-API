1bf306f8d90d51fcc6720d0b360c8d460a77eca3
rb <rkrb@tyfonmail.se>
2024-08-25 15:28:52 +0200

    otherwise error contactInformation is required despite being filled in

--- MovieCard-API/Models/Director.cs
+++ MovieCard-API/Models/Director.cs
@@ -5,7 +5,7 @@ public class Director
 {
     public int Id { get; set; }
     public int ContactInformationId { get; set; }
-    public ContactInformation ContactInformation { get; set; }
+    public ContactInformation? ContactInformation { get; set; }
     public string Name { get; set; }
     public DateTime Birthday { get; set; }
     public ICollection<Movie> Movies { get; set; }
