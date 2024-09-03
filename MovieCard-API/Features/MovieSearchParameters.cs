e678015316321ea304dcc082c7dd9bf732b9c94d
RB <rkrb@tyfonmail.se>
2024-09-03 17:15:41 +0200

    Movie search parameters

---
+++ MovieCard-API/Features/MovieSearchParameters.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieCard_API.Features;
+
+public  class MovieSearchParameters
+{
+    public string? Title { get; set; }
+    public string? Genre { get; set; }
+    public string? ActorName { get; set; }
+    public string? DirectorName { get; set; }
+    public DateTime? ReleaseDateFrom { get; set; }
+    public DateTime? ReleaseDateTo { get; set; }
+    public bool IncludeActors { get; set; } = false;
+}
