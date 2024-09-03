96a822c3be680b21250e85c34e282f175ab39f4e
rb <rkrb@tyfonmail.se>
2024-09-03 10:47:52 +0200

    mapped actor, director and genre to their respective DTO

--- MovieCard-API/Data/MapperProfile.cs
+++ MovieCard-API/Data/MapperProfile.cs
@@ -10,6 +10,22 @@ public class MapperProfile : Profile
     {
         CreateMap<Movie, MovieDTO>().ReverseMap();
         CreateMap<Movie, MovieDTO>().ConstructUsing(
-            src => new MovieDTO(src.Director, src.Actors, src.Genres, src.Title,src.Rating, src.ReleaseDate,src.Description));
+            src => new MovieDTO(src.Director, src.Actors, src.Genres, src.Title, src.Rating, src.ReleaseDate,
+                src.Description));
+        CreateMap<Actor, ActorDTO>()
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Birthday, opt => opt.MapFrom(src => src.Birthday))
+            .ForMember(dest => dest.Movies, opt => opt.MapFrom(src => src.Movies));
+
+        CreateMap<Director, Director>().ReverseMap();
+        CreateMap<Director, DirectorDTO>()
+            .ForMember(dest => dest.ContactInformation, opt => opt.MapFrom(src => src.ContactInformation))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Birthday, opt => opt.MapFrom(src => src.Birthday))
+            .ForMember(dest => dest.Movies, opt => opt.MapFrom(src => src.Movies));
+
+        CreateMap<Genre, GenreDTO>()
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.Movies, opt => opt.MapFrom(src => src.Movies));
     }
 }
