6cf9874d4fbe25912cd2dad12bda3714d1b43fe5
RB <rkrb@tyfonmail.se>
2024-09-06 16:58:35 +0200

    to just add one movie

--- MovieCard-API/DTOs/MovieCreateDTO.cs
+++ MovieCard-API/DTOs/MovieCreateDTO.cs
@@ -6,7 +6,7 @@ public record MovieCreateDTO
     public int Rating { get; set; }
     public DateTime ReleaseDate { get; set; }
     public string Description { get; set; }
-    public int DirectorId { get; set; }
-    public int ActorIds { get; set; }
-    public int GenreIds { get; set; }
+    public int? DirectorId { get; set; }
+    public IEnumerable<int>? ActorIds { get; set; }
+    public IEnumerable<int>? GenreIds { get; set; }
 }
