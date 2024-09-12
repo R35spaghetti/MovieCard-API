3df0018a42c8e38115aff5e3ef18034873cc9865
rb <rkrb@tyfonmail.se>
2024-09-12 21:21:09 +0200

    directorID doesn't have ?

--- MovieCard-API/Models/Movie.cs
+++ MovieCard-API/Models/Movie.cs
@@ -3,7 +3,7 @@
 public class Movie
 {
     public int Id { get; set; }
-    public int? DirectorId { get; set; }
+    public int DirectorId { get; set; }
     public Director Director { get; set; }
     public ICollection<Actor> Actors { get; set; }
     public ICollection<Genre> Genres { get; set; }
